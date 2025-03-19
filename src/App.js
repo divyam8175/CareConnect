6eedc942fc11720635829d7c6578681c416b5929
DIVYAM VERMA <divyamverma90@gmail.com>
2025-03-19 13:53:42 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";  
-import Services from "./components/Services";  
-import Login from "./pages/Login";  
-import Signup from "./pages/Signup";  
+import HeroSection from "./components/HeroSection";
+import Services from "./components/Services";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import AiAssistant from "./components/AIHealthAssistant";
 import Footer from "./components/Footer";
-import FeaturedDoctors from "./components/FeaturedDoctors"; 
+import FeaturedDoctors from "./components/FeaturedDoctors";
 const App = () => {
   return (
     <Router>
-      <Navbar /> 
+      <Navbar />
       <Routes>
-        <Route path="/home" element={
-          <>
-            <HeroSection />  
-            <Services />  
-          </>
-        } />
-
-        {/* Authentication Pages */}
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-
-        {/* Default Route - Redirect to Home */}
-        <Route path="*" element={
-          <>
-            <HeroSection />
-            <Services />
-          </>
-        } />
       </Routes>
+      <HeroSection />
+      <Services />
       <AiAssistant />
       <FeaturedDoctors />
       <Footer />
