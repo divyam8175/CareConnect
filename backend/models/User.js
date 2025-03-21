d7ddffd98b5a7cc9c7a9fbb09dec59ca541a74ad
DIVYAM VERMA <113888422+divyam8175@users.noreply.github.com>
2025-03-21 20:23:28 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, required: true },
+  role: { type: String, enum: ['doctor', 'patient'], required: true },
+  otp: { type: String } // Optional field for OTP verification
 }, { timestamps: true });
 
 // userSchema.pre("save", async function (next) {
@@ -19,5 +20,4 @@ const userSchema = new mongoose.Schema({
 // userSchema.methods.matchPassword = async function (enteredPassword) {
 //   return await bcrypt.compare(enteredPassword, this.password);
 // };
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
